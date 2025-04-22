5f759fecaa56ab66b21745e3703b9de95e6712c1
lav kumar <lavkumar062@gmail.com>
2025-04-23 01:17:40 +0530

    backend

--- Backend/SignUp.js
+++ Backend/SignUp.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: 'https://to-do-application-three-xi.vercel.app/', credentials: true }));
+app.use(cors({ origin: 'https://to-do-application-three-xi.vercel.app', credentials: true }));
 
 await connectDB();
 
